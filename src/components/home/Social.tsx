ec841dd85159f631b504fd5132da5b43cd5729cf
Herman <hmurauyou@icloud.com>
2025-01-14 19:57:52 +0300

    COMPONENTS: home added, styles added.

---
+++ src/components/home/Social.tsx
@@ -0,0 +1,19 @@
+import Switch from "./Switch";
+
+export default function Social() {
+    return (
+        <div className="home__social">
+            <a href="https://www.credly.com/users/hmurauyou" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-amazon"></i>
+            </a>
+            <a href="https://leetcode.com/u/myumbr3lla" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className='bx bx-code-alt'></i>
+            </a>
+            <a href="https://github.com/interl1nk" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-github-alt"></i>
+            </a>
+
+            <Switch />
+        </div>
+    );
+}
