6e4c2f3774fa520652de5f257c96552146ab5efc
Herman <168635773+interlink-chief@users.noreply.github.com>
2025-02-17 13:41:56 +0300

    Update Social.tsx

--- src/components/home/Social.tsx
+++ src/components/home/Social.tsx
@@ -9,7 +9,7 @@ export default function Social() {
             <a href="https://leetcode.com/u/myumbr3lla" className="home__social-icon" target="_blank" rel="noreferrer">
                 <i className='bx bx-code-alt'></i>
             </a>
-            <a href="https://github.com/interl1nk" className="home__social-icon" target="_blank" rel="noreferrer">
+            <a href="https://interlink-chief.github.io/Herman" className="home__social-icon" target="_blank" rel="noreferrer">
                 <i className="uil uil-github-alt"></i>
             </a>
 
